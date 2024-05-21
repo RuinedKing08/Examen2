d61f3593bae6733d9648c4e03e82a6a603fa78d4
RuinedKing08 <newapheliosplayer@gmail.com>
2024-05-21 18:43:42 -0500

    Mateo Leon

---
+++ Examen2/Assets/BlackEnemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackEnemy : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyType = "Black";
+    }
+
+}
